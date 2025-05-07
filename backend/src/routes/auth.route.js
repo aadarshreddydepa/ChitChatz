3f718ff5b21fd5692750a88a2449c852a12c962c
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-07 07:31:05 +0530

    Made the code very clean by using the controllers

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,13 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/",(req,res)=>{
     res.send("Hello and Welcome to ChitChatz");
 })
-router.get("/signup",(req,res)=>{
-    res.send("SignUp Route");
-})
-router.get("/login",(req,res)=>{
-    res.send("Login Route");
-})
-router.get("/logout",(req,res)=>{
-    res.send("Logout Route");
-})
+router.get("/signup",signup)
+router.get("/login",login)
+router.get("/logout",logout)
 
 export default router
