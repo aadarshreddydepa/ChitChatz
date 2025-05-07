5a61e0a565e5ee82fdd9669a99b6238585ff3605
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-07 07:50:09 +0530

    Made it clean

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,10 @@
+
 export function signup(req,res){
     res.send("Signup Route");
 }
-
 export function login(req,res){
     res.send("Login Route");
 }
 export function logout(req,res){
     res.send("Logout Route");
 }
-// export function signup(req,res){
-//     res.send("Signup Route");
-// }
-
