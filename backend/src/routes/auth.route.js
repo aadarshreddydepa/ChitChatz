0a3fa64cbde2eea47cae425d02d4e6c6c7959b35
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-07 07:21:34 +0530

    Routes Folder Created

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.send("Hello and Welcome to my App");
+})
+router.get("/signup",(req,res)=>{
+    res.send("SignUp Route");
+})
+router.get("/login",(req,res)=>{
+    res.send("Login Route");
+})
+router.get("/logout",(req,res)=>{
+    res.send("Logout Route");
+})
+
+export default router
