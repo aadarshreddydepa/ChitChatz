01bf82949a2311c8048858d1fe14b8d61249af37
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-13 08:35:30 +0530

    Function called in the Server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
 dotenv.config();
 
 const app = express();
@@ -12,5 +13,5 @@ app.use("/api/auth",authRoutes);
 
 app.listen(PORT , () =>{
     console.log(`Server is Running at port number ${PORT}`);
-    
+    connectDB();
 })
