ef0e6478dbbeae34f19e9eee4ad094b706ab8798
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-14 08:37:04 +0530

    model for User has been implemented

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const UserSchema = new mongoose.Schema({
     fullname:{
         type:String,
@@ -57,4 +57,19 @@ const UserSchema = new mongoose.Schema({
 
 
 const User = mongoose.model("User",UserSchema);
+
+//pre hook 
+//password hashing 
+
+UserSchema.pre("save",async function(next){
+    if ( !this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 export default User;
