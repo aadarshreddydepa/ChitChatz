4373a9f426a8cb8aace6ef094fed0adad17908b3
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-14 08:39:18 +0530

    the usage of express.json() has been implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-app.get("/",(req,res)=>{
-    res.send("Hello and Welcome to ChitChatz");
+app.get("/", (req, res) => {
+  res.send("Hello and Welcome to ChitChatz");
 });
-app.use("/api/auth",authRoutes);
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT , () =>{
-    console.log(`Server is Running at port number ${PORT}`);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log(`Server is Running at port number ${PORT}`);
+  connectDB();
+});
