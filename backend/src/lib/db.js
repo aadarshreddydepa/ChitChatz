0c5592826f61b2045f71fd1c3c2df59befe6ef23
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-14 08:36:24 +0530

    User functionality implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import mogoose from "mongoose";
 
-const connectDB = async () =>{
+export const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`Mongo DB connection establised at : ${conn.connection.host}`);
     } catch (error) {
+        console.log("Error in connection Establishement : ",error);
         process.exit(1);
     }
 }
---
+++ backend/src/models/User.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required : true
+    },
+    email:{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password:{
+        type : String,
+        required : true,
+        minlength : 6
+    },
+
+    bio : {
+        type : String,
+        default : ""
+    },
+
+    profilePic : {
+        type : String,
+        default : ""
+    },
+
+    nativeLanguage : {
+        type : String,
+        default : ""
+    },
+
+    lraeningLanguage : {
+        type : String,
+        default : ""
+    },
+
+    location : {
+        type : String,
+        default : ""
+    },
+
+    isOnBoarded : {
+        type : Boolean,
+        default : false
+    },
+
+    friends : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    ]
+
+},{timestamps:true})
+
+
+const User = mongoose.model("User",UserSchema);
+export default User;
