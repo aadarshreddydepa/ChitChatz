a20934d8bb964b341d6a486cdf460bf8514fa186
Aadarsh Reddy Depa <aadarshreddydepa@gmail.com>
2025-05-14 08:38:16 +0530

    The logic for taking input from body has been implemented here

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,51 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+export async function signup(req, res) {
+  const { email, password, fullname } = req.body;
+  try {
+    if (!email || !fullname || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
 
-export function signup(req,res){
-    res.send("Signup Route");
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must contain atleast 6 charcters",
+      });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const exsistingUser = await User.findOne({email});
+    if(exsistingUser){
+        return res.status(400).json(
+            {
+                message:"This email is already in use , please use a different email"
+            }
+        );
+    }
+
+    const idx = Math.floor(Math.random() * 100 ) + 1;
+    const randomAvatar  = `https://avatar.iran.liara.run/public/${idx}.png`
+
+    const newUser = new User.create({
+        email,
+        fullname,
+        password,
+        profilePic: randomAvatar
+    })
+
+    
+  } catch (error) {}
 }
-export function login(req,res){
-    res.send("Login Route");
+export function login(req, res) {
+  res.send("Login Route");
 }
-export function logout(req,res){
-    res.send("Logout Route");
+export function logout(req, res) {
+  res.send("Logout Route");
 }
